c29ac93c21cf6a57f450785d78c9091dea14fb76
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-09 06:06:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,20 @@
 def instantiate_new_array
   arr = []
 end
+
+def array_with_two_elements
+  arr = ["element1", "element2"]
+end
+
+def first_element (arr)
+  arr[0]
+end
+
+def third_element (arr)
+  arr[2]
+end
+
+def last_element (arr)
+  arr[-1]
+end
+
