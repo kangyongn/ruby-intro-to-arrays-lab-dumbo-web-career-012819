9aa63358c0b7b69fd16096af1dd5cecfb1139bb1
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-09 06:08:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,3 +18,14 @@ def last_element (arr)
   arr[-1]
 end
 
+def first_element_with_array_methods (arr)
+  arr.first
+end
+
+def last_element_with_array_methods (arr)
+  arr.last
+end
+
+def length_of_array (arr)
+  arr.length
+end
